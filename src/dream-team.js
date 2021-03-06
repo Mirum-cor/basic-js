d37bc5667055c6941afa5422d63fef92cc77c0ee
Mirum-cor <laetitia-caritas@mail.ru>
2021-03-06 19:37:02 +0300

    feat: solved dream-team task (tests are passed)

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members === 'object' && members && members.length) {
+    const trimedMembers = members.map((member) => {
+      if (typeof member === 'string') {
+        return member.trim().toUpperCase();
+      }
+    });
+    const sortedMembers = trimedMembers.sort();
+    const result = sortedMembers.map((member) => {
+      if (typeof member === 'string') {
+        return member.slice(0, 1);
+      }
+    });
+    return result.join('');
+  } else {
+    return false;
+  }
 };
