dba87d950df731a788fcd17cf40e011f25734d16
Mirum-cor <laetitia-caritas@mail.ru>
2021-03-08 15:37:01 +0300

    feat: solved carbon-dating.js (tests are passed)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const sampleFloat = parseFloat(sampleActivity);
+  if (
+    !sampleActivity ||
+    typeof sampleActivity !== 'string' ||
+    !sampleFloat ||
+    sampleFloat > 15 ||
+    sampleFloat <= 0
+  ) {
+    return false;
+  } else {
+    const coefficient =
+      Math.trunc(Math.log(2) * 1000) / 1000 / HALF_LIFE_PERIOD;
+    const age = Math.log(MODERN_ACTIVITY / sampleFloat) / coefficient;
+    return Math.ceil(age);
+  }
 };
