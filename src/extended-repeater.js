2afa137bd070fc9aee3bc15f8ad8fe13e2a7420d
Mirum-cor <laetitia-caritas@mail.ru>
2021-03-08 21:57:27 +0300

    feat: solved extended-repeater.js (tests are passed)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let {
+    repeatTimes,
+    separator = '+',
+    addition,
+    additionRepeatTimes = 0,
+    additionSeparator = '|',
+  } = options;
+  str = String(str);
+  if (Boolean(addition)) {
+    addition = String(addition);
+  }
+  let result = '';
+  if (!repeatTimes) {
+    result += str;
+    if (!additionRepeatTimes) {
+      if (addition) {
+        result += addition;
+      }
+    } else {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        result += addition;
+        if (j < additionRepeatTimes - 1) {
+          result += additionSeparator;
+        }
+      }        
+    }
+  }
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+    if (additionRepeatTimes) {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        result += addition;
+        if (j < additionRepeatTimes - 1) {
+          result += additionSeparator;
+        }
+      }
+    } else {
+      if (typeof addition === 'string') {
+        result += addition;
+      } 
+    }
+    if (i < repeatTimes - 1) {
+      result += separator;
+    }
+  }
+  return result;
 };
-  
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,25 +2,66 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   getLength() {
+    // return this.chain.split('~~').slice(0, -1).length;
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
   },
   addLink(value) {
+/*     if (this.chain) {
+      if (typeof value) {
+        this.chain += `( ${value} )~~`;
+      } else {
+        this.chain += `(  )~~`;
+      }
+    } else {
+      if (typeof value) {
+        this.chain = `( ${value} )~~`;
+      } else {
+        this.chain = `(  )~~`;
+      }
+    }
+    return this; */
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
   },
   removeLink(position) {
+/*     if (this.chain && this.getLength() >= position && position) {
+      let separator = '~~';
+      if (!this.chain.split('~~').slice(position, -1).length) {
+        separator = '';
+      }
+      this.chain = this.chain.split('~~').slice(0, position - 1).join('~~') + separator + this.chain.split('~~').slice(position, -1).join('~~');
+      if (this.chain[0] === '~') {
+        this.chain = this.chain.slice(2);
+      }
+      if (this.chain[this.getLength() - 1] !== '~') {
+        this.chain += '~~';
+      }
+      return this;
+    } else {
+      throw new Error('There is not such a position!');
+    } */
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
   },
   reverseChain() {
+/*     if (this.chain) {
+      this.chain = this.chain.split('~~').slice(0, -1).reverse().join('~~') + '~~';
+    }
+    return this; */
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
   },
   finishChain() {
+    // return this.chain.split('~~').slice(0, -1).join('~~');
     throw new CustomError('Not implemented');
-    // remove line with error and write your code here
   }
 };
 
 module.exports = chainMaker;
+
+// console.log(chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain());
+// '( NaN )~~( null )~~( true )~~( false )~~( 1.233 )'
+
+// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
+// ( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )
+
+// console.log(chainMaker.addLink(7).addLink(9).addLink(null));
+// console.log(chainMaker.addLink(7).addLink(9).reverseChain());
+// console.log(chainMaker.addLink(3).addLink().addLink(5).removeLink(3).reverseChain().finishChain());
