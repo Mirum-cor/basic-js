8250bdc1268cfd8e6f96eefff8582be68375ed12
Mirum-cor <laetitia-caritas@mail.ru>
2021-03-08 17:45:18 +0300

    feat: solved what-season.js (tests are passed)

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,43 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date) {
+    if (Object.keys(date).length !== 0) {
+      throw new Error("It's not a date!");
+    }
+    if (typeof date === 'string') {
+      switch (date) {
+        case 'winter':
+        case 'spring':
+        case 'summer':
+        case 'autumn':
+          return date;
+      }
+    }
+    const dateStr = String(date);
+    const regexp = /\w{3}\s\w{3}\s\d{2}\s\d{4}\s\d{2}\:\d{2}\:\d{2}/gi;
+    if (dateStr.match(regexp)) {
+      const month = dateStr.split(' ')[1];
+      switch (month) {
+        case 'Dec':
+        case 'Jan':
+        case 'Feb':
+          return 'winter';
+        case 'Mar':
+        case 'Apr':
+        case 'May':
+          return 'spring';
+        case 'Jun':
+        case 'Jul':
+        case 'Aug':
+          return 'summer';
+        case 'Sep':
+        case 'Oct':
+        case 'Nov':
+          return 'autumn';
+      }
+    } else {
+      throw new Error("It's not a date!");
+    }
+  } else {
+    return 'Unable to determine the time of year!';
+  }
 };
