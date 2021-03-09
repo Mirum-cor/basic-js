98cac295bbf9d26190c459ad0e5a47fe252ee431
Mirum-cor <laetitia-caritas@mail.ru>
2021-03-09 20:59:04 +0300

    refactor: {number of tests in last commit was wrong} partially solved recursive-depth.js (not all tests are passed [2/4])

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -12,7 +12,6 @@ const chainMaker = {
       this.chain.push(`(  )`);
     }
     return this;
-    // throw new CustomError('Not implemented');
   },
   removeLink(position) {
     if (this.chain && this.getLength() >= position && position) {
@@ -23,31 +22,16 @@ const chainMaker = {
     } else {
       throw new Error('There is not such a position!');
     }
-    // throw new CustomError('Not implemented');
   },
   reverseChain() {
     if (this.chain) {
       this.chain.reverse();
     }
     return this;
-    // throw new CustomError('Not implemented');
   },
   finishChain() {
     return this.chain.join('~~');
-    // throw new CustomError('Not implemented');
   },
 };
 
 module.exports = chainMaker;
-
-// console.log(chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain());
-// '( NaN )~~( null )~~( true )~~( false )~~( 1.233 )'
-
-// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
-// ( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )
-
-// console.log(chainMaker.addLink(7).addLink(9).addLink(null).removeLink(1).finishChain());
-// console.log(chainMaker.addLink(7).addLink(9).reverseChain()/* .finishChain() */);
-// console.log(chainMaker.addLink(3).addLink().addLink(5).removeLink(3).reverseChain().finishChain());
-
-// node src/simple-chain.js
